7d9f436d388612e3302a5cf3de844fa1061f907b
Paul Solomon <paultsolomon@gmail.com>
2023-06-28 00:12:46 +0000

    orgin

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
